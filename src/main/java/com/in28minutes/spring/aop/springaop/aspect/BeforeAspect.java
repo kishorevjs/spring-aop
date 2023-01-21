4a6ae975c5615b57b0d080cb2a1707cfa0322067
kishorevjs <jayasaikishore@gmail.com>
2023-01-21 12:02:05 -0800

    Implementing Before Intercept calls in Spring AOP Used @Before, JoinPoint for intercepting and Logger for logging

---
+++ src/main/java/com/in28minutes/spring/aop/springaop/aspect/BeforeAspect.java
@@ -0,0 +1,23 @@
+package com.in28minutes.spring.aop.springaop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class BeforeAspect {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    //What kind of method calls I would intercept
+    //execution (* PACKAGE.*.*(..))
+
+    @Before("execution(* com.in28minutes.spring.aop.springaop.business.*.*(..))")
+    public void before(JoinPoint joinPoint){
+        logger.info("Intercepting method call - {}", joinPoint);
+    }
+}
